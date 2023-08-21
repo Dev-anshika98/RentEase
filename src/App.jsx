fc9e17ea4eed231c657e25a1be5fe38a3eb85b33
Aditya Varshney <aditya.varshneymail@gmail.com>
2023-08-21 11:50:26 +0530

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,12 @@ import { useState } from 'react'
 import Navbar from './Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
-  
-switch (window.location.pathname){
-  case "/":
-    break
-    case "/pricing":
-      break
-      case "/about":
-      break
+ 
 
-}
 
   return (
   <>
+  
   <Navbar />
   </>
   );
